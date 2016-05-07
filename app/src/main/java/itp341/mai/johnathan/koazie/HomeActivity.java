346372fbd25523f6f8d0b95f9e216635ee49942c
Johnny <johnathm@usc.edu>
2016-05-07 15:19:10 -0700

    first commit

---
+++ app/src/main/java/itp341/mai/johnathan/koazie/HomeActivity.java
@@ -0,0 +1,27 @@
+package itp341.mai.johnathan.koazie;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment f = fm.findFragmentById(R.id.fragment_container);
+
+        if (f == null) {
+            f = HomeFragment.newInstance();
+        }
+
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, f);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/itp341/mai/johnathan/koazie/HomeFragment.java
@@ -0,0 +1,39 @@
+package itp341.mai.johnathan.koazie;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return v;
+    }
+}
